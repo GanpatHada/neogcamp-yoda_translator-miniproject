d973abb35377966dddf83f128edcad7779a1f6aa
GanpatHada <hadaganpat42@gmail.com>
2022-08-31 01:44:17 +0530

    fix bug and improve ux

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ document.getElementById("mytext").addEventListener('change', (event) => {
     mytext = event.target.value;
 });
 translate.addEventListener("click", async () => {
+    if(mytext.length===0)return alert("Please write something");
     translate.innerHTML = "Translating...";
     finaltext.innerHTML = "Translating...";
+    finaltext.style.color = "indigo";
     try {
         const result = await fetch("https://api.funtranslations.com/translate/yoda.json?text=" + mytext); // Now this will wait till it finished
         let translatedtext = await result.json(); //this will also wait till it finished
-        finaltext.style.color = "indigo";
         finaltext.innerHTML = translatedtext.contents.translated;
-        translate.innerHTML = "Translated";
     } catch (e) {
         finaltext.style.color = "#a50000";
         finaltext.innerHTML = "Something error has been occured.Please try again";
