0b45933cd9fe5786fbfe871e5b1511cd9c1e5379
GanpatHada <hadaganpat42@gmail.com>
2022-08-31 13:24:15 +0530

    bug fix improve ux

--- index.js
+++ index.js
@@ -2,32 +2,76 @@ let mytext = "";
 let translate = document.querySelector("#translate-btn");
 let clear = document.querySelector("#clear-btn");
 let finaltext = document.querySelector("#finaltext");
+let btndisabled = false;
 document.getElementById("mytext").addEventListener('change', (event) => {
     mytext = event.target.value;
 });
-translate.addEventListener("click", async () => {
-    if(mytext.length===0)return alert("Please write something");
+document.getElementById("mytext").addEventListener('input', () => {
+    if (btndisabled) {
+        translate.innerHTML = "Translate";
+        translate.style.color = "white";
+        translate.style.cursor = "pointer"
+        translate.removeAttribute('disabled');
+    }
+    finaltext.style.color = "#6b6b6b";
+    finaltext.innerHTML = "Translated text will appear Here";
+});
+
+translate.addEventListener("click", () => {
+   
+    return onTranslateClick();
+
+})
+clear.addEventListener("click", () => {
+    
+    return onClearClick();
+
+})
+function onTranslateClick() {
+    if (mytext.length === 0) return alert("Please write something");
     translate.innerHTML = "Translating...";
     finaltext.innerHTML = "Translating...";
     finaltext.style.color = "indigo";
-    try {
-        const result = await fetch("https://api.funtranslations.com/translate/yoda.json?text=" + mytext); // Now this will wait till it finished
-        let translatedtext = await result.json(); //this will also wait till it finished
-        finaltext.innerHTML = translatedtext.contents.translated;
-    } catch (e) {
-        finaltext.style.color = "#a50000";
-        finaltext.innerHTML = "Something error has been occured.Please try again";
-        translate.innerHTML = "Translate";
-        console.log(e);
-    }
-
-})
-clear.addEventListener("click", async () => {
-    finaltext.style.color = "indigo";
+    translate.setAttribute('disabled', true);
+    btndisabled = true;
+    translate.style.color = "grey";
+    translate.style.cursor = "not-allowed";
+    onApiCall();
+}
+async function onApiCall() {
+    setTimeout(() => {
+        try {
+            // const result = await fetch("https://api.funtranslations.com/translate/minion.json?text=" + mytext); // Now this will wait till it finished
+            // let translatedtext = await result.json(); 
+            // let translatedtext = await result.json();
+            // finaltext.innerHTML = translatedtext.contents.translated;
+            finaltext.innerHTML="Hello MOTO";
+            finaltext.style.color="indigo";
+            translate.innerHTML = "Translated";
+        } catch (e) {
+            finaltext.style.color = "#a50000";
+            finaltext.innerHTML = "Something error has been occured.Please try again";
+            translate.innerHTML = "Translated";
+            console.log(e);
+        }
+    },5000);
+}
+function onClearClick(){
+    //function called after clear button pressed
+    finaltext.style.color = "#6b6b6b";
     finaltext.innerHTML = "Translated text will appear Here";
-    document.querySelector("#mytext").value = "";
+    mytext = "";
+    document.getElementById("mytext").value = "";
     document.querySelector("#mytext").focus();
-    translate.innerHTML = "Translate"
+    if (btndisabled) {
+        translate.innerHTML = "Translate";
+        translate.style.color = "white";
+        translate.style.cursor = "pointer"
+        translate.removeAttribute('disabled');
+    }
+}
 
 
-})
+
+
+
